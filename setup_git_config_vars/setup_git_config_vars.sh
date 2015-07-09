f3ba8e5f737f61118ecf7dcafa6d336a9d198323
James Brisland <james.brisland@open.ac.uk>
2015-07-09 11:58:19 +0100

    Tweaking how the apache folder is setup

--- setup_git_config_vars/setup_git_config_vars.sh
+++ setup_git_config_vars/setup_git_config_vars.sh
@@ -14,6 +14,8 @@ if [ ! -z "${GIT_PREVIOUS_COMMIT}" ]; then
     echo GIT_PREVIOUS_COMMIT=${GIT_PREVIOUS_COMMIT} >> $config_file
 fi
 
+exit 1
+
 #- Copy the directory into a unique one in /var/www/html so that if we rebuild this job process it's always running
 #- on a unique checkout of the code
 echo "Setup apache www folder /var/www/html/${job_start_time}_${config_file}"

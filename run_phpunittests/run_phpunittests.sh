dff9175c700f058837558490839d4aa8acee32a6
James Brisland <james.brisland@open.ac.uk>
2015-07-20 15:37:54 +0100

    Minor tweaks. No change in functionality

--- run_phpunittests/run_phpunittests.sh
+++ run_phpunittests/run_phpunittests.sh
@@ -17,9 +17,11 @@ set +e
 #                        raise error or just warning (dlft).
 # $extraconfig: Extra settings that will be injected ti config.php
 
+# Check params passed down
+echo "Unique Job ID: ${unique_job_ident}"
+echo "Setup build start time: ${setup_build_start_time}"
+
 # Include the config file!
-echo "00. Setup Job start time: ${setup_build_start_time}"
-echo Unique Job Ident: ${unique_job_ident}
 config_file_path=${JENKINS_HOME}/git_repositories/config_files/${setup_build_start_time}_${unique_job_ident}
 . ${config_file_path}
 

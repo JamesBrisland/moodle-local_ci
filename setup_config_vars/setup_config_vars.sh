f5912f9ee570d48e9c85c388ac201171f80e11f9
James Brisland <james.brisland@open.ac.uk>
2015-07-20 11:25:54 +0100

    Bugs... Including file that doesn't exist!

--- setup_config_vars/setup_config_vars.sh
+++ setup_config_vars/setup_config_vars.sh
@@ -11,9 +11,7 @@ mkdir ${JENKINS_HOME}/git_repositories/config_files
 echo "Unique Job ID: ${unique_job_ident}"
 echo "Setup build start time: ${setup_build_start_time}"
 
-# Include the config file!
 config_file_path=${JENKINS_HOME}/git_repositories/config_files/${setup_build_start_time}_${unique_job_ident}
-. ${config_file_path}
 
 touch $config_file_path
 
